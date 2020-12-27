8054277791dcf7d71a7fd6a4892b56c5ba3aa910
Rajat Paliwal <pali4raj@gmail.com>
2020-12-27 21:05:25 +0530

    added newer version

--- src/db_actions.py
+++ src/db_actions.py
@@ -1,9 +1,21 @@
 import os
 import sqlite3
 from sqlite3 import Error
-
+import random
 import config as cf
 
+role = [
+    "Quality Assurance Specialist",
+    "Tester",
+    "Software Engineer",
+    "Software Developer",
+    "Quality Analyst",
+    "Business Analyst",
+    "Business Associate",
+    "Junior Assitant",
+    "Senior Assistant",
+    "Data Scientist"
+]
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
@@ -18,5 +30,15 @@ def create_connection(db_file):
             conn.close()
 
 
+def dummy_db(type_job, location="Pune"):
+    n = random.randint(0, 9)
+    template = "Hi, Cognologix is looking for "+role[n]+". This role requires you to be proficient in software as well as testing side." \
+                                                        " If you would like to apply please say apply for the job."
+    return template
+
+
+"""
 if __name__ == '__main__':
     create_connection(os.path.join(cf.db_path, "information.db"))
+"""
+
